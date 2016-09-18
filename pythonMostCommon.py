ad469139cd7ffb59b02e8812c90b148efd70a515
bsy167508 <ahm.rimer@live.com>
2016-09-18 19:23:53 +0530

    adding final before test

--- pythonMostCommon.py
+++ pythonMostCommon.py
@@ -1,31 +1,3 @@
-from collections import deque
-d = deque()
-d.append(1)
-print d
-d.appendleft(2)
-print d
-d.clear()
-print d
-d.extend('1')
-print d
-d.extendleft('234')
-print d
-print d.count('1')
-d.pop()
-print d
-d.popleft()
-print d
-d.extend('7896')
-print d
-d.remove('2')
-print d
-d.reverse()
-print d
-d.rotate(3)
-print d
-d = deque()
-for _ in range(int(raw_input())):
-    inp = raw_input().split()
-    getattr(d, inp[0])(*[inp[1]] if len(inp) > 1 else [])
-for item in d:
-    print item,
+from collections import Counter
+string = sorted(Counter(raw_input()).items(), key= lambda x: (-x[1],x[0]))[:3]
+print("\n".join(x[0]+" "+str(x[1]) for x in string))
