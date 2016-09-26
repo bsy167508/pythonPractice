16a416102d8bcbe20ca7fcdf6256f0cb56808628
bsy167508 <ahm.rimer@gmail.com>
2016-09-26 22:57:09 +0530

    update

--- project/test.py
+++ project/test.py
@@ -1,17 +1,25 @@
 import MySQLdb
 
-<<<<<<< HEAD
-db = MySQLdb.connect('localhost','root','12345678','mydb')
-db.autocommit(True)
-cur = db.cursor()
+def curbal(id):
+    cur.execute("select balance from customer where id=%s" % id )
+    bal = cur.fetchone()
+    return bal[0]
 
-''
-=======
+def recharge(balance, id):
+    cur.execute("update customer set balance=%s+%s where id=%s" % (curbal(id), balance, id))
+    
+def rechargeAll(balance, id):
+    cur.execute("update customer set balance=%s+%s" % (curbal(id), balance))
+
+def purchase(itemprice, cid, oid):
+    cur.execute("update customer set balance=%s-%s where id=%s" % (curbal(cid), itemprice, cid))
+    cur.execute("insert into transactions(cid, oid, price) values(%s, %s, %s)" % (cid, oid, itemprice))
+    
 # Open database connection
 db = MySQLdb.connect("localhost","root","12345678","mydb" )
+db.autocommit(True)
 # prepare a cursor object using cursor() method
 cur = db.cursor() 
-db.autocommit(True)
 
 try:
     cur.execute("SELECT VERSION()")
@@ -29,6 +37,7 @@ sql = """create table customer(
             id int(20) NOT NULL auto_increment,
             name varchar(30),
             is_stud int(2),
+            balance int,
             primary key(id) )"""
             
 cur.execute(sql)
@@ -52,5 +61,7 @@ cur.execute(sql)
 
 #Populate initial data to database
 cur.executemany("insert into outlet(name) values(%s)", ['scoops', 'cafeteria', 'ccd', 'lipton'])
-cur.executemany("insert into customer(name, is_stud) values(%s, %s)", [('ram', 1), ('shyam', 0), ('seeta', 1), ('lipton', 0)])
->>>>>>> 20a801d59e6b0b43c4c5c3edd097d99a18bdf288
+cur.executemany("insert into customer(name, is_stud, balance) values(%s, %s, %s)", [('ram', 1, 0), ('shyam', 0, 0), ('seeta', 1, 0), ('lipton', 0, 0)])
+
+rechargeAll(1000, 1)
+purchase(20, 2, 3)
