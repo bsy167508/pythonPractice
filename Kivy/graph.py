9a49d123eed4e6456db24a5b13161ddc71c27cfd
bsy167508 <ahm.rimer@gmail.com>
2016-10-03 21:52:03 +0530

    update

---
+++ Kivy/graph.py
@@ -0,0 +1,30 @@
+from math import sin
+from kivy.garden.graph import Graph, MeshLinePlot
+from kivy.app import App
+
+
+graph = Graph(xlabel='X',
+              ylabel='Y', 
+              x_ticks_minor=5,
+              x_ticks_major=25, 
+              y_ticks_major=1,
+              y_grid_label=True, 
+              x_grid_label=True, 
+              padding=5,
+              x_grid=True, 
+              y_grid=True, 
+              xmin=-0, 
+              xmax=100, 
+              ymin=-1, 
+              ymax=1)
+plot = MeshLinePlot(color=[1, 0, 0, 1])
+plot.points = [(x, sin(x / 10.)) for x in range(0, 101)]
+graph.add_plot(plot)
+
+class TestApp(App):
+
+    def build(self):
+        return graph
+
+if __name__ == '__main__':
+    TestApp().run()
--- Kivy/main.py
+++ Kivy/main.py
@@ -1,15 +1,105 @@
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager,Screen, FadeTransition
+from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 
-class LoginScreen(BoxLayout):
+Builder.load_string("""
+<LoginScreen>:
+    id: loginscreen
+    uname: unameinput
+    passw: passinput
+    action: loginbutton
+    BoxLayout:
+        orientation: 'vertical'
+        
+        BoxLayout:
+            id: bottombox
+            size_hint_y: 0.40
+        
+        BoxLayout:
+            id: focus
+            size_hint_y: 0.15
+            
+            BoxLayout:
+                id: sidebox1
+                size_hint_x: 0.15
+            
+            BoxLayout:
+                id:middlebox
+                orientation: 'vertical'
+                size_hint_x:0.70
+                canvas.before: 
+                    Color: 
+                        rgb: 0.6, 0.6, 0.6
+                    Rectangle:
+                        size: self.size
+                        pos: self.pos
+                            
+                BoxLayout:
+                    id: unamebox
+                    Label:
+                        id:unamelabel
+                        text: 'Username : '
+                    TextInput:
+                        id:unameinput
+                        
+                BoxLayout:
+                    id: passbox
+                    
+                    Label:
+                        id:passlabel
+                        text: 'Password : '
+                    TextInput:
+                        id:passinput
+                        password:True
+                        
+                Button:
+                    id: loginbutton
+                    text: 'Login'
+                    on_press: loginscreen.validation()
+                    
+                    
+                       
+                        
+            BoxLayout:
+                id:sidebox2
+                size_hint_x:0.15
+                
+        BoxLayout:
+            id: bottombox
+            size_hint_y: 0.45
+
+<Dashboard>:
+    BoxLayout:
+        Label:
+            text: 'Hello'
+""")
+
+
+class LoginScreen(Screen):
+    
     def validation(self):
-        pass
-
-class Root(BoxLayout):
+        if self.action.text=='Login':
+            if self.uname.text=='ahm.rimer' and self.passw.text=='password':
+                    sm.switch_to(Dashboard(name='screen 2'))
+            else:
+                    self.action.text='Wrong credentials, click to try again.'
+        else:
+            self.uname.text=''
+            self.passw.text=''
+            self.action.text='Login'
+            
+class Dashboard(Screen):
     pass
 
-class DisplayApp(App):
-    pass
+sm = ScreenManager()
+sm.add_widget(LoginScreen(name='screen 1'))
+sm.add_widget(Dashboard(name='screen2'))
 
-if __name__=='__main__':
-    DisplayApp().run()
+class TestApp(App):
+
+    def build(self):
+        return sm
+
+if __name__ == '__main__':
+    TestApp().run()
