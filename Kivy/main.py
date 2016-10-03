6a9b89399d02a61b4924a632589154404bc09983
bsy167508 <ahm.rimer@gmail.com>
2016-10-03 15:29:39 +0530

    update

--- Kivy/main.py
+++ Kivy/main.py
@@ -1,17 +1,15 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
 
-class WeatherRoot(BoxLayout):
+class LoginScreen(BoxLayout):
+    def validation(self):
+        pass
+
+class Root(BoxLayout):
     pass
 
-class AddLocationForm(BoxLayout):
-    search_input = ObjectProperty()
-    def search_location(self):
-        print "The user searched for '{}'".format(self.search_input.text)
-
-class WeatherApp(App):
+class DisplayApp(App):
     pass
 
 if __name__=='__main__':
-    WeatherApp().run()
+    DisplayApp().run()
